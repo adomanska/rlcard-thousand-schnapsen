551783e95a4d457763f4ea9fb093cf2d9a679ca5
Andżelika Domańska <adomanska96@gmail.com>
2020-08-19 23:36:49 +0200

    Add complete state encoding with tests

--- rlcard_thousand_schnapsen/envs/thousand_schnapsen.py
+++ rlcard_thousand_schnapsen/envs/thousand_schnapsen.py
@@ -1,10 +1,10 @@
-from typing import Dict, List, Sequence
+from typing import Dict, List, Sequence, Tuple, Optional, Set
 
 import numpy as np
 from rlcard.envs import Env
 
 from rlcard_thousand_schnapsen.games.thousand_schnapsen import Game
-from rlcard_thousand_schnapsen.games.thousand_schnapsen.constants import CARDS_PER_SUIT_COUNT, CARDS_COUNT
+from rlcard_thousand_schnapsen.games.thousand_schnapsen.constants import CARDS_PER_SUIT_COUNT, CARDS_COUNT, SUITS_COUNT
 from rlcard_thousand_schnapsen.utils import Card
 
 
@@ -13,6 +13,7 @@ class ThousandSchnapsenEnv(Env):
     def __init__(self, config: Dict):
         self.name = 'thousand-schnapsen'
         self.game = Game()
+        self.state_shape = [6 * CARDS_COUNT + 2 * SUITS_COUNT]
         super().__init__(config)
 
     def get_payoffs(self):
@@ -33,7 +34,48 @@ class ThousandSchnapsenEnv(Env):
         pass
 
     def _extract_state(self, state):
-        pass
+        stock: List[Tuple[int, Card]] = state['stock']
+        current_player: int = state['current_player']
+        hand: List[Card] = state['hand']
+        players_used: Sequence[List[Card]] = state['players_used']
+        active_marriage: Optional[str] = state['active_marriage']
+        used_marriages: Set[str] = state['used_marriages']
+
+        first_player_id = stock[0][0] if len(stock) > 0 else current_player
+        opponents_indices = [
+            (first_player_id + i) % self.game.get_player_num()
+            for i in range(self.game.get_player_num())
+        ]
+        opponents_indices = [
+            player_id for player_id in opponents_indices
+            if player_id != current_player
+        ]
+
+        obs = np.zeros(self.state_shape[0], dtype=int)
+        start_index = 0
+        start_index = self._encode(obs, self._encode_cards_set(hand),
+                                   start_index, CARDS_COUNT)
+        start_index = self._encode(
+            obs, self._encode_cards_set(players_used[current_player]),
+            start_index, CARDS_COUNT)
+        start_index = self._encode(
+            obs, self._encode_cards_set(players_used[opponents_indices[0]]),
+            start_index, CARDS_COUNT)
+        start_index = self._encode(
+            obs, self._encode_cards_set(players_used[opponents_indices[1]]),
+            start_index, CARDS_COUNT)
+        start_index = self._encode(
+            obs, self._encode_card(stock[0][1] if len(stock) > 0 else None),
+            start_index, CARDS_COUNT)
+        start_index = self._encode(
+            obs, self._encode_card(stock[1][1] if len(stock) > 1 else None),
+            start_index, CARDS_COUNT)
+        start_index = self._encode(obs, self._encode_marriage(active_marriage),
+                                   start_index, SUITS_COUNT)
+        self._encode(obs, self._encode_marriages(used_marriages), start_index,
+                     SUITS_COUNT)
+
+        return {'obs': obs, 'legal_actions': self._get_legal_actions()}
 
     def _decode_action(self, action_id):
         """ Decode Action id to the action in the game.
@@ -55,8 +97,36 @@ class ThousandSchnapsenEnv(Env):
         """
         return [action.__hash__() for action in self.game.get_legal_actions()]
 
+    def _encode(self, code: np.array, chunk: np.array, start_index: int,
+                length: int) -> int:
+        end_index = start_index + length
+        code[start_index:end_index] = chunk
+        return end_index
+
     def _encode_cards_set(self, cards: Sequence[Card]) -> np.array:
         cards_indices = [card.__hash__() for card in cards]
         code = np.zeros(CARDS_COUNT, dtype=int)
         code[cards_indices] = 1
+        return code
+
+    def _encode_card(self, card: Optional[Card]) -> np.array:
+        code = np.zeros(CARDS_COUNT, dtype=int)
+        if card is None:
+            return code
+        card_index = card.__hash__()
+        code[card_index] = 1
+        return code
+
+    def _encode_marriages(self, suits: Set[str]) -> np.array:
+        suits_indices = [Card.valid_suit.index(suit) for suit in suits]
+        code = np.zeros(SUITS_COUNT, dtype=int)
+        code[suits_indices] = 1
+        return code
+
+    def _encode_marriage(self, suit: Optional[str]) -> np.array:
+        code = np.zeros(SUITS_COUNT, dtype=int)
+        if suit is None:
+            return code
+        suit_index = Card.valid_suit.index(suit)
+        code[suit_index] = 1
         return code
--- tests/envs/test_thousand_schnapsen.py
+++ tests/envs/test_thousand_schnapsen.py
@@ -6,13 +6,16 @@ from ddt import ddt, data, unpack
 
 from rlcard_thousand_schnapsen.core import *
 from rlcard_thousand_schnapsen.envs import make
-from rlcard_thousand_schnapsen.games.thousand_schnapsen.constants import CARDS_COUNT
+from rlcard_thousand_schnapsen.games.thousand_schnapsen.constants import CARDS_COUNT, SUITS_COUNT
 from rlcard_thousand_schnapsen.utils import Card
 
 
 @ddt
 class TestThousandSchnapsenEnv(unittest.TestCase):
-    env = make('thousand-schnapsen', config={'seed': 0})
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.env = make('thousand-schnapsen', config={'seed': 0})
+        self.env.reset()
 
     @data((2, Card(Spades, Queen)), (19, Card(Hearts, Jack)),
           (10, Card(Clubs, Ten)), (17, Card(Diamonds, Ace)))
@@ -43,5 +46,77 @@ class TestThousandSchnapsenEnv(unittest.TestCase):
         expected[[2, 10, 17, 19]] = 1
 
         result = self.env._encode_cards_set(cards)
+
+        self.assertTrue(np.allclose(expected, result))
+
+    def test_encode_card_when_card_is_none(self):
+        expected = np.zeros(CARDS_COUNT, dtype=int)
+
+        result = self.env._encode_card(None)
+
+        self.assertTrue(np.allclose(expected, result))
+
+    def test_encode_card_when_card_is_defined(self):
+        expected = np.zeros(CARDS_COUNT, dtype=int)
+        expected[10] = 1
+
+        result = self.env._encode_card(Card(Clubs, Ten))
+
+        self.assertTrue(np.allclose(expected, result))
+
+    def test_encode_marriage_when_suit_is_none(self):
+        expected = np.zeros(SUITS_COUNT, dtype=int)
+
+        result = self.env._encode_marriage(None)
+
+        self.assertTrue(np.allclose(expected, result))
+
+    def test_encode_marriage_when_suit_is_defined(self):
+        expected = np.zeros(SUITS_COUNT, dtype=int)
+        expected[2] = 1
+
+        result = self.env._encode_marriage(Diamonds)
+
+        self.assertTrue(np.allclose(expected, result))
+
+    def test_encode_marriages(self):
+        expected = np.zeros(SUITS_COUNT, dtype=int)
+        expected[[1, 3]] = 1
+
+        result = self.env._encode_marriages({Clubs, Hearts})
+
+        self.assertTrue(np.allclose(expected, result))
+
+    def test_encode(self):
+        code = np.zeros(10, dtype=int)
+        chunk = np.ones(4, dtype=int)
+        start_index = 2
+        length = 4
+        expected = np.zeros(10, dtype=int)
+        expected[2:6] = 1
+
+        end_index = self.env._encode(code, chunk, start_index, length)
+
+        self.assertTrue(np.allclose(expected, code))
+        self.assertEqual(6, end_index)
+
+    def test_extract_state(self):
+        state = {
+            'stock': [(1, Card(Diamonds, Jack))],
+            'active_marriage': Hearts,
+            'used_marriages': {Hearts, Diamonds},
+            'hand': [Card(Diamonds, Queen),
+                     Card(Spades, Nine)],
+            'players_used': [[], [Card(Diamonds, Jack)], []],
+            'current_player': 2
+        }
+        expected = np.zeros(152, dtype=int)
+        expected[[0, 14]] = 1  # hand
+        expected[61] = 1  # used by 1
+        expected[109] = 1  # stock
+        expected[147] = 1  # active marriage
+        expected[[150, 151]] = 1  # used marriages
+
+        result = self.env._extract_state(state)['obs']
 
         self.assertTrue(np.allclose(expected, result))
