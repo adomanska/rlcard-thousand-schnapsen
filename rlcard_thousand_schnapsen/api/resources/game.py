a823976fb28c31b17ed82e3f58b48d0652d0eb48
Andżelika Domańska <adomanska96@gmail.com>
2020-08-31 14:45:41 +0200

    Add body parsing

--- rlcard_thousand_schnapsen/api/resources/game.py
+++ rlcard_thousand_schnapsen/api/resources/game.py
@@ -1,9 +1,23 @@
+from flask import request
 from flask_restful import Resource
+
+from rlcard_thousand_schnapsen.api.models import GameSetup
+from rlcard_thousand_schnapsen.envs import make
 
 
 class Game(Resource):
     def __init__(self, model: str):
         self._model = model
+        self._env = make('thousand-schnapsen',
+                         config={
+                             'seed': 0,
+                             'force_zero_sum': False
+                         })
 
     def get(self):
         return {'hello': self._model}
+
+    def post(self):
+        game_setup: GameSetup = GameSetup.from_dict(request.json)
+        print(game_setup.player_types)
+        return {}, 200
